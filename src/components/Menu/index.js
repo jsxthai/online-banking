7b8346c5eac652b8f6d4f035309238e6e1c61b29
thai pham <jsxthai@gmail.com>
2020-11-30 20:39:30 +0700

    css

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
@@ -19,26 +19,36 @@ const useStyles = makeStyles((theme) => ({
     secondaryColor: {
         color: "#f50057",
     },
+    activeColor: {
+        background: " linear-gradient(to right, #396afc, #2948ff)",
+        color: "#f4f4f4",
+    },
 }));
 
 export default function Menu() {
     let history = useHistory();
-
+    const [isActive, setIsActive] = useState("");
     const classes = useStyles();
+
     function handleClickDashBoard() {
         history.push("/dashboard");
+        setIsActive("das");
     }
     function handleClickInterTransfer() {
         history.push("/internal-transfer");
+        setIsActive("nal");
     }
     function handleClickInterbank() {
         history.push("/interbank");
+        setIsActive("int");
     }
     function handleClickDebtReminder() {
         history.push("/debt-reminder");
+        setIsActive("deb");
     }
     function handleClickRecipientList() {
         history.push("/recipient-list");
+        setIsActive("rec");
     }
     const dispatch = useDispatch();
     function handleClickLogout() {
@@ -47,31 +57,51 @@ export default function Menu() {
     return (
         <>
             <List>
-                <ListItem button onClick={handleClickDashBoard}>
+                <ListItem
+                    button
+                    onClick={handleClickDashBoard}
+                    className={isActive === "das" ? classes.activeColor : ""}
+                >
                     <ListItemIcon>
                         <DashboardIcon />
                     </ListItemIcon>
                     <ListItemText primary={"Dasboard"} />
                 </ListItem>
-                <ListItem button onClick={handleClickInterTransfer}>
+                <ListItem
+                    button
+                    onClick={handleClickInterTransfer}
+                    className={isActive === "nal" ? classes.activeColor : ""}
+                >
                     <ListItemIcon>
                         <AccountBalanceIcon />
                     </ListItemIcon>
                     <ListItemText primary={"Internal Transfer"} />
                 </ListItem>
-                <ListItem button onClick={handleClickInterbank}>
+                <ListItem
+                    button
+                    onClick={handleClickInterbank}
+                    className={isActive === "int" ? classes.activeColor : ""}
+                >
                     <ListItemIcon>
                         <LanguageIcon />
                     </ListItemIcon>
                     <ListItemText primary={"Interbank"} />
                 </ListItem>
-                <ListItem button onClick={handleClickDebtReminder}>
+                <ListItem
+                    button
+                    onClick={handleClickDebtReminder}
+                    className={isActive === "deb" ? classes.activeColor : ""}
+                >
                     <ListItemIcon>
                         <MoneyOffIcon />
                     </ListItemIcon>
                     <ListItemText primary={"Debt Reminder"} />
                 </ListItem>
-                <ListItem button onClick={handleClickRecipientList}>
+                <ListItem
+                    button
+                    onClick={handleClickRecipientList}
+                    className={isActive === "rec" ? classes.activeColor : ""}
+                >
                     <ListItemIcon>
                         <PlaylistAddIcon />
                     </ListItemIcon>
