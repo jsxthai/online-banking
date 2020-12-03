e68734e0cf0425c1c1a2a0a7f9a55dbfa3292eee
thai pham <jsxthai@gmail.com>
2020-12-03 15:24:58 +0700

    filter history

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -11,55 +11,147 @@ import Title from "../common/Title";
 import { fetchHistory } from "../../actions/history";
 import { useDispatch, useSelector } from "react-redux";
 import dateFormat from "dateformat";
+import ImportExportIcon from "@material-ui/icons/ImportExport";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import { SwitchCamera } from "@material-ui/icons";
+import { InputLabel, MenuItem, Select } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+    receiveColor: {
+        color: "#4287f5",
+        fontWeight: "bold",
+    },
+});
 const History = () => {
+    const classes = useStyles();
+
     const accountNumber = useSelector(
         (state) => state.loginReducer.accountNumber
     );
-    const history = useSelector((state) => state.history);
+    let history = useSelector((state) => state.history);
     const [rowsTable, setRowsTable] = useState("");
+    const [filterLists, setFilterLists] = useState([]);
+    const [selectType, setSelectType] = useState("all");
+    const [filter, setFilter] = useState({
+        sortDate: true,
+        sortMoney: true,
+        sortType: true,
+    });
 
-    console.log("account number", accountNumber);
+    // console.log("account number", accountNumber);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchHistory(accountNumber));
     }, [accountNumber]);
-    console.log("history", history);
+    // console.log("history", history);
 
     useEffect(() => {
         if (history.transactionLists.length > 0) {
-            const rows = history.transactionLists.map((item, key) => (
-                <TableRow key={key}>
-                    <TableCell component="th" scope="row">
-                        {key + 1}
-                    </TableCell>
-                    <TableCell align="left">{item.accountDest}</TableCell>
-                    <TableCell align="center">
-                        <b>
-                            {item.mount.toLocaleString("it-IT", {
-                                // style: "currency",
-                                currency: "VND",
-                            })}{" "}
-                            VND
-                        </b>
-                    </TableCell>
-                    <TableCell align="center">{item.typeTrans}</TableCell>
-                    <TableCell align="center">{item.detail}</TableCell>
-                    <TableCell align="right">
-                        {/* {dateFormat(item.date, "HH:MM:ss' - 'yyyy-mm-dd")} */}
-                        {dateFormat(item.date, "HH:MM' - 'yyyy-mm-dd")}
-                    </TableCell>
-                </TableRow>
-            ));
-            setRowsTable(rows);
+            setFilterLists(history.transactionLists);
         }
-    }, [history]);
+    }, [history.transactionLists]);
+
+    useEffect(() => {
+        const rows = filterLists.map((item, key) => (
+            <TableRow key={key}>
+                <TableCell component="th" scope="row">
+                    {key + 1}
+                </TableCell>
+                <TableCell align="left">{item.accountDest}</TableCell>
+                <TableCell align="center">
+                    <b>
+                        {item.mount.toLocaleString("it-IT", {
+                            // style: "currency",
+                            currency: "VND",
+                        })}{" "}
+                        VND
+                    </b>
+                </TableCell>
+                <TableCell
+                    className={
+                        item.typeTrans === "receive" ? classes.receiveColor : ""
+                    }
+                    align="center"
+                >
+                    {item.typeTrans}
+                </TableCell>
+                <TableCell align="center">
+                    {item.detail || "no comments"}
+                </TableCell>
+                <TableCell align="right">
+                    {/* {dateFormat(item.date, "HH:MM:ss' - 'yyyy-mm-dd")} */}
+                    {dateFormat(item.date, "HH:MM' - 'yyyy-mm-dd")}
+                </TableCell>
+            </TableRow>
+        ));
+        setRowsTable(rows);
+    }, [filterLists, filter]);
+
+    const handleSortDate = () => {
+        const lists = filterLists.sort((a, b) => {
+            if (a.date < b.date) return filter.sortDate ? 1 : -1;
+            if (a.date > b.date) return !filter.sortDate ? 1 : -1;
+            return 0;
+        });
+        setFilterLists(lists);
+        setFilter({
+            sortDate: !filter.sortDate,
+        });
+    };
+
+    const handleSortMoney = () => {
+        const lists = filterLists.sort((a, b) => {
+            if (a.mount < b.mount) return filter.sortMoney ? 1 : -1;
+            if (a.mount > b.mount) return !filter.sortMoney ? 1 : -1;
+            return 0;
+        });
+        setFilterLists(lists);
+        setFilter({
+            sortMoney: !filter.sortMoney,
+        });
+    };
+
+    const handleSelectType = (e) => {
+        const value = e.target.value;
+        setSelectType(value);
+        if (value === "all") {
+            setFilterLists(history.transactionLists);
+            return;
+        }
+        // filter
+        // value === type of data in database
+        const lists = history.transactionLists.filter((item) => {
+            return item.typeTrans === value;
+        });
+        setFilterLists(lists);
+        setFilter({
+            sortType: !filter.sortType,
+        });
+    };
 
     return (
         <>
             <Grid item xs={12} sm={10} md={8}>
-                <Title>History</Title>
+                <Title>My history transaction</Title>
+                <div style={{ height: "20px" }}></div>
+
+                <InputLabel id="label ">Type</InputLabel>
+                <Select
+                    labelId="label"
+                    id="select"
+                    value={selectType}
+                    style={{ width: "150px" }}
+                    onChange={handleSelectType}
+                >
+                    <MenuItem value="all">All</MenuItem>
+                    <MenuItem value="transfer">Transfer</MenuItem>
+                    <MenuItem value="receive">Receive</MenuItem>
+                </Select>
+                <div style={{ height: "20px" }}></div>
                 <TableContainer component={Paper}>
                     <Table aria-label="caption table">
                         {/* <caption>A basic table example with a caption</caption> */}
@@ -69,10 +161,32 @@ const History = () => {
                                 <TableCell align="left">
                                     Recipient number
                                 </TableCell>
-                                <TableCell align="center">Money</TableCell>
+                                <TableCell
+                                    align="center"
+                                    onClick={handleSortMoney}
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    Money
+                                    {filter.sortMoney ? (
+                                        <KeyboardArrowDownIcon fontSize="small" />
+                                    ) : (
+                                        <KeyboardArrowUpIcon fontSize="small" />
+                                    )}
+                                </TableCell>
                                 <TableCell align="center">Type</TableCell>
                                 <TableCell align="center">Detail</TableCell>
-                                <TableCell align="center">Date</TableCell>
+                                <TableCell
+                                    align="center"
+                                    onClick={handleSortDate}
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    Date
+                                    {filter.sortDate ? (
+                                        <KeyboardArrowDownIcon fontSize="small" />
+                                    ) : (
+                                        <KeyboardArrowUpIcon fontSize="small" />
+                                    )}
+                                </TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
