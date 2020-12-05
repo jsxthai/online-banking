6fe98af449975be7e6a266ad9570b9d181c3d4d7
thai pham <jsxthai@gmail.com>
2020-12-05 15:10:01 +0700

    hot fix set header

--- src/api/login.js
+++ src/api/login.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
-axios.defaults.headers.post["Content-Type"] = "text/plain";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 const uri = process.env.REACT_APP_URI_SERVER || "http://localhost:7777";
 
