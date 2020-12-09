0582f4c274643b9f8a52897a430f4a6fb3f84d24
thai pham <jsxthai@gmail.com>
2020-12-09 16:12:17 +0700

    set header withCredentials true

--- src/api/loginWithJWT.js
+++ src/api/loginWithJWT.js
@@ -3,7 +3,11 @@ axios.defaults.withCredentials = true;
 const uri = process.env.REACT_APP_URI_SERVER || "http://localhost:7777";
 
 export const loginWithJWT = (accessToken) => {
-    return axios.post(`${uri}/api/login-use-jwt`, {
-        accessToken,
-    });
+    return axios.post(
+        `${uri}/api/login-use-jwt`,
+        { withCredentials: true },
+        {
+            accessToken,
+        }
+    );
 };
