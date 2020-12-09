439b6909856e7cdd6cad0372e2238ad67b6de264
thai pham <jsxthai@gmail.com>
2020-12-09 16:03:00 +0700

    set token in body

--- src/api/loginWithJWT.js
+++ src/api/loginWithJWT.js
@@ -4,6 +4,6 @@ const uri = process.env.REACT_APP_URI_SERVER || "http://localhost:7777";
 
 export const loginWithJWT = (accessToken) => {
     return axios.post(`${uri}/api/login-use-jwt`, {
-        // accessToken,
+        accessToken,
     });
 };
